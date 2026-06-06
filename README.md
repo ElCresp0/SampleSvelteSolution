274c4a1bc439c0e06097c62f5bb3b8593fe275d1
ElCresp0 <jacky8203@protonmail.com>
2026-06-06 22:03:00 +0200

    state fo art

--- README.md
+++ README.md
@@ -8,8 +8,14 @@
 
 ```bash
 nvm use 25.2.1
+
+# creating a new project
 npx sv@0.15.3 create --template minimal --types ts --add prettier eslint --install npm svelte-tutorial
 cd svelte-tutorial
+
+# setting up an existing project
+cd svelte-tutorial
+npm install # npm i
 ```
 
 ## run
--- svelte-tutorial/src/components/GenericStatefulComponent.svelte
+++ svelte-tutorial/src/components/GenericStatefulComponent.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
+    // change in an object with $state affects the displayed content
+    let numbersWithState = $state([1, 2, 3, 4]);
+    let numbersWithoutState = [1, 2, 3, 4];
 
+    // $derived object reacts to changes in it's base stateful object and affects the displayed content accordingly
+    let total = $derived(
+        numbersWithState.reduce(
+            (accumulator, currentNb) => accumulator + currentNb,
+            0,
+        ),
+    );
+
+    let totalWithoutState = $derived(
+        numbersWithoutState.reduce(
+            (accumulator, currentNb) => accumulator + currentNb,
+            0,
+        ),
+    );
+
+    function pushNumber() {
+        numbersWithState.push(numbersWithState.length + 1);
+        numbersWithoutState.push(numbersWithoutState.length + 1);
+        console.log(
+            "stateful:",
+            $state.snapshot(numbersWithState),
+            "unstateful:",
+            numbersWithoutState,
+        );
+    }
+
+    $inspect(numbersWithState); // will print the inspected object on every update
 </script>
 
+<p>numbersWithState: {numbersWithState}</p>
+<p>numbersWithState: {numbersWithState.join(" + ")} = {total}</p>
+<p>numbersWithoutState: {numbersWithoutState}</p>
 <p>
-    hello
-    and bye
+    numbersWithoutState: {numbersWithoutState.join(" + ")} = {totalWithoutState}
 </p>
+
+<button onclick={pushNumber}>push a number</button>

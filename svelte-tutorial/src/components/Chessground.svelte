73053cabebdc9ec333b84e8fd6c0040b525b66bc
ElCresp0 <jacky8203@protonmail.com>
2026-07-08 22:30:35 +0200

    add chess move event

--- svelte-tutorial/src/components/Chessground.svelte
+++ svelte-tutorial/src/components/Chessground.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
     import { Chessground } from "@lichess-org/chessground";
+    import { Chess } from "chess.js";
     import { onMount } from "svelte";
+	import type { Key } from "@lichess-org/chessground/types";
 
     import "$lib/assets/Chessground/chessground.css";
     import "$lib/assets/Chessground/theme.css";
 
+
+    function userMove(from: Key, to: Key):void {
+        console.debug(`move ${from} -> ${to}`)
+        chess.move({from: from, to: to})
+    }
+
     let {board="blue", pieces="merida"} = $props();
     let chessDiv: HTMLElement;
-    const config = {};
+    const config = {
+        "events": {
+            "move": userMove,
+        },
+    };
+    const chess = new Chess();
 
     onMount(() => {
-        const ground = $state(Chessground(chessDiv!, config));
+        const ground = Chessground(chessDiv!, config);
         // TODO: preserve the state through page refreshing
     });
 </script>

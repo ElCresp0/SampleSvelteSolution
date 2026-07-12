15d94ec6648da9f8fb9e9fd79091058ba35391b2
ElCresp0 <jacky8203@protonmail.com>
2026-07-12 11:42:33 +0200

    read pages from props and migrate styles to component

--- svelte-tutorial/src/components/Navbar.svelte
+++ svelte-tutorial/src/components/Navbar.svelte
@@ -1,44 +1,54 @@
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/generic-subpage">Generic Subpage</a></li>
-        <li><a href="/chessboard">Chessboard</a></li>
-        <li><a href="/about">About</a></li>
-    </ul>
-</nav>
+<script lang="ts">
+    let { pages }: {pages: {address: string, title: string}[]} = $props();
+</script>
+
+<div class="navbar">
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            {#each pages as page}
+                <li><a href="{page.address}">{page.title}</a></li>
+            {/each}
+        </ul>
+    </nav>
+</div>
 
 <style>
+
+.navbar {
+	position: sticky;
+	top: 0;
+	width: 100%;
+	z-index: 1000;
+}
+
 nav {
-  /* position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 1000; */
-  background-color: #333;
-  padding: 10px 20px;
+    background-color: #333;
+    padding: 10px 20px;
 }
 
 nav ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 nav ul li {
-  margin: 0 15px;
+    margin: 0 15px;
 }
 
 nav ul li a {
-  color: white;
-  text-decoration: none;
-  padding: 10px 15px;
-  display: block;
+    color: white;
+    text-decoration: none;
+    padding: 10px 15px;
+    display: block;
 }
 
 nav ul li a:hover {
-  background-color: #575757;
-  border-radius: 4px;
+    background-color: #575757;
+    border-radius: 4px;
 }   
 </style>
